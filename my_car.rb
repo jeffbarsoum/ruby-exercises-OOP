4fb8f2dfda0606b88d8b1cdb5a967e0e128d33cc
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-04-22 07:41:29 -0400

    Exercise 2 & 3 attr_reader & paint_car method

--- my_car.rb
+++ my_car.rb
@@ -1,5 +1,7 @@
 class MyCar
-  attr_accessor :year, :color, :model, :name
+  # attr_writer :year, :color, :model
+  attr_reader :speed, :started, :name
+  attr_accessor :year, :color, :model
 
   def initialize year, color, model
     err = <<-STRING
@@ -17,47 +19,38 @@ class MyCar
     @name = "#{self.year} #{self.color} #{self.model}"
   end
 
-  def speed
-    @speed
+  def get_new_car year, color, model
+    err = <<-STRING
+    That's the same car tho??
+    Cars don't age or deprecate in ruby terminal land..
+    Why limit yourself?
+    Change it up, be different!
+
+    STRING
+    same_car = year == self.year && color == self.color && model = self.model
+    puts err if same_car
+    return if same_car
+
+    self.year = year unless year == self.year
+    self.color = color unless color == self.color
+    self.model = model unless model == self.model
+    self.name = "#{self.year} #{self.color} #{self.model}"
+    puts "Congrats on your new #{self.name}!"
   end
 
-  def started
-    @started
-  end
+  def paint_car color
+    err = <<-STRING
+    That's the same color tho??
+    Paint doesn't fade or crack in ruby terminal land..
+    Why limit yourself?
+    Change it up, be different!
 
-  def name=(year, color, model)
-    @name = "#{self.year} #{self.color} #{self.model}"
-  end
-
-  def name
-    @name
-  end
-
-  def year=(y)
-    name(self.year, self.color, self.model)
-    @year = y
-  end
-
-  def year
-    @year
-  end
-
-  def color=(c)
-    name(self.year, self.color, self.model)
-    @color = c
-  end
-
-  def color
-    @color
-  end
-
-  def model=(m)
-    name(self.year, self.color, self.model)
-    @model = m
-  end
-
-  def model
-    @model
+    STRING
+    puts err if color == self.color
+    return if color == self.color
+    self.color = color
+    self.name = "#{self.year} #{self.color} #{self.model}"
+    puts "Ohh, #{self.color}, huh? Bold!"
   end
 
   def increase_speed acceleration
@@ -105,6 +98,10 @@ class MyCar
     @speed = s
   end
 
+  def name=(n)
+    @name = n
+  end
+
 end
 
 
@@ -124,3 +121,22 @@ end
   car.speed
   car.stop_car
   car.speed
+  # car.color = "Green" # should return NoMethod error, since it's reader only
+  car.name
+  # car.year = "2015" # should return NoMethod error, since it's reader only
+  car.year
+  car.color
+  car.model
+  car.get_new_car '2010', 'Blue', 'Honda' # should return error message
+  # car.get_new_car year: '2010' # should return error message
+  # car.get_new_car model: 'Chevrolet' # should return error message
+  car.get_new_car '2015', 'Green', 'Subaru'
+  car.name
+  car.year
+  car.color
+  car.paint_car 'Green' # should return error msg
+  car.paint_car 'Red'
+  car.name
+  car.year
+  car.color
+  car.model
